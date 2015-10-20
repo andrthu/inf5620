d5984d85ec7d5fa03d976aff22f3968293b68cd4
Andreas Thune <andrthu@student.matnat.uio.no>
2015-10-21 00:04:28 +0200

    Description of changes.

--- uke6/test_2Dsolver.py
+++ uke6/test_2Dsolver.py
@@ -1,5 +1,5 @@
 from wave2D_solver import *
-from scitools.std import zeros,cos,amax,pi,sqrt,log,meshgrid
+from scitools.std import zeros,cos,amax,pi,sqrt,log,exp,sin
 
 def test_constant():
 
@@ -7,7 +7,7 @@ def test_constant():
     
     #define functions that give costant solution
     I=lambda x,y: k
-    q=lambda x,y: 34
+    q=lambda x,y: 34 + x
     
     #define some variables
     Lx = 10
@@ -21,8 +21,10 @@ def test_constant():
     Ny = int(round(Ly/dt))
     ue =zeros((Nx+1,Ny+1)) +k
 
-    u,x,y,t,error=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='vector')
-    #print amax(abs(u-ue)), len(x),y[:,-1]
+    u,x,y,t,error=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='vector')
+    print 'Test constant solution. Exercise 3.1'
+    print '||abs(u-ue)||: ', amax(abs(u-ue))
+    print
     assert amax(abs(u-ue))<10**(-16)
 
 def test_plug():
@@ -51,18 +53,45 @@ def test_plug():
             return 0
 
     
-    
-    u,x,y,t = solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar')
+    #solve numerically
+    u,x,y,t,err = solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar')
+
+    #find exact solution at time T
     x1 = x+T
     x2 = x-T
-
     for i in range(Nx+1):
         for j in range(Ny+1):
             ue[i,j]=0.5*(I(x1[i],0) + I(x2[i],0))
+    
+    #calculate error at time t=T
+    e1 = amax(abs(u-ue))
+    
 
+    #Do the exact same for plug wave in y direction:
+    def I(x,y):
+        if abs(y-Ly/2.)<0.5:
+            return 2.1
+        else:
+            return 0
+    #Numerical solution
+    u,x,y,t,err = solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar')
     
+    #find exact solution at time T
+    y1 = x+T
+    y2 = x-T    
+    for i in range(Nx+1):
+        for j in range(Ny+1):
+            ue[i,j]=0.5*(I(0,y1[j]) + I(0,y2[j]))
+
+
+    #calculate error at time t=T
+    e2 = amax(abs(u-ue))
     
-    assert amax(abs(u-ue))<10**(-16)
+    print 'Plug error in x ||u(x,y,T)-ue(x,y,T)||: ',e1
+    print 'Plug error in y ||u(x,y,T)-ue(x,y,T)||: ',e2
+    print
+    
+    assert e1<10**(-16) and e2<10**(-16)
 
 
 
@@ -109,18 +138,96 @@ def test_undampedWaves():
     for i in range(val):
         v='vector'
         #solve eqation
-        u,x,y,t,e=solver(I,None,None,q,0,Lx,Ly,dt*step**(i),T,C,mode=v,ue=ue)
+        u,x,y,t,e=solver(I,None,None,q,0,Lx,Ly,dt*step**(i),T,C,1,mode=v,ue=ue)
         
         
         E[i]=e
-        print E[i]
+        
     #find convergence rate between diffrent dt values
     r =zeros(val-1)
     r = log(E[1:]/E[:-1])/log(step)
 
-    print E,log(E[1:]/E[:-1])/log(step)
+    
+    print "Test convergence for undamped waves:"
+    for i in range(val):
+        if i==0:
+            print "dt: ",dt," Error: ",E[i]
+        else:
+            print "dt: ",dt*step**(i)," Error: ",E[i], "rate of con.: ", r[i-1]
+        
+    
     #requiere close to 2 in convergence rate for last r.
-    assert abs(r[-1]-2)<0.01    
+    assert abs(r[-1]-2)<0.01 
+
+def test_manufactured():
+
+    A=1
+    B=0
+    mx=1
+    my=1
+    
+    
+    b=1
+    c=1.1
+
+    #define some variables
+    Lx = 2.
+    Ly = 2.
+    T = 1
+    C = 0.3
+    dt= 0.1
+
+    #help varabeles
+    kx = pi*mx/Lx
+    ky = pi*my/Ly
+    w=1
+
+    #Exact solution
+    ue = lambda x,y,t: A*cos(x*kx)*cos(y*ky)*cos(t*w)*exp(-c*t)
+    I = lambda x,y: A*cos(x*kx)*cos(y*ky)
+    V = lambda x,y: -c*A*cos(x*kx)*cos(y*ky)
+    q = lambda x,y: x**2+y**2
+
+    f = lambda x,y,t:A*(-b*(c*cos(t*w) + w*sin(t*w))*cos(kx*x)*cos(ky*y) + c**2*cos(kx*x)*cos(ky*y)*cos(t*w) + 2*c*w*sin(t*w)*cos(kx*x)*cos(ky*y) + kx**2*(x**2 + y**2)*cos(kx*x)*cos(ky*y)*cos(t*w) + 2*kx*x*sin(kx*x)*cos(ky*y)*cos(t*w) + ky**2*(x**2 + y**2)*cos(kx*x)*cos(ky*y)*cos(t*w) + 2*ky*y*sin(ky*y)*cos(kx*x)*cos(t*w) - w**2*cos(kx*x)*cos(ky*y)*cos(t*w))*exp(-c*t)
+    
+
+    
+    
+    
+
+    #factor dt decreeses per step
+    step=0.5
+    #number of steps I want to do
+    val=5
+    #array to store errors
+    E=zeros(val)
+    
+    
+    
+    for i in range(val):
+        v='vector'
+        #solve eqation
+        u,x,y,t,e=solver(I,V,f,q,b,Lx,Ly,dt*step**(i),T,C,8,mode=v,ue=ue)
+        
+        
+        E[i]=e
+        
+    #find convergence rate between diffrent dt values
+    r =zeros(val-1)
+    r = log(E[1:]/E[:-1])/log(step)
+
+    print
+    print "Convergence rates for manufactured solution:" 
+    for i in range(val):
+        if i==0:
+            print "dt: ",dt," Error: ",E[i]
+        else:
+            print "dt: ",dt*step**(i)," Error: ",E[i], "rate of con.: ", r[i-1]
+        
+    
+    #requiere "close" to 2 in convergence rate for last r.
+    assert abs(r[-1]-2)<0.5
+
     
 def constructed_bugs():
     k=2.718281828459045
@@ -138,20 +245,23 @@ def constructed_bugs():
     Ny = int(round(Ly/dt))
     ue =zeros((Nx+1,Ny+1)) +k
 
-    u1,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar',bug='bug1')
-    u2,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar',bug='bug2')
-    u3,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar',bug='bug3')
-    u4,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar',bug='bug4')
-    u5,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,mode='scalar',bug='bug5')
-
+    u1,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar',bug='bug1')
+    u2,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar',bug='bug2')
+    u3,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar',bug='bug3')
+    u4,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar',bug='bug4')
+    u5,x,y,t=solver(I,None,None,q,0,Lx,Ly,dt,T,C,1,mode='scalar',bug='bug5')
+    
+    print "Test bugs:" 
     print "bug1 gives error: ",amax(abs(u1-ue))
     print "bug2 gives error: ",amax(abs(u2-ue))
     print "bug3 gives error: ",amax(abs(u3-ue))
     print "bug4 gives error: ",amax(abs(u4-ue))
     print "bug5 gives error: ",amax(abs(u5-ue))
+    print
 
 if __name__ == "__main__":
-    #test_constant()
-    #constructed_bugs()
-    #test_plug()
+    test_constant()
+    constructed_bugs()
+    test_plug()
     test_undampedWaves()
+    test_manufactured()
--- uke6/wave2D_solver.py
+++ uke6/wave2D_solver.py
@@ -1,29 +1,54 @@
 import vectorization as vec 
-from scitools.std import linspace, newaxis,zeros,amax
+from numpy import linspace, newaxis,zeros,amax,sqrt,ceil
+from scalar import *
 
-def solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None,ue=None):
-    
-    Nt = int(round(T/dt))
-    t = linspace(0, Nt*dt, Nt+1)
-    dt = t[1]-t[0]
 
-    dx = dt/C 
-    Nx = int(round(Lx/dx))
-    x = linspace(0, Nx*dx, Nx+1)
-    dx=x[1]-x[0]
-    xv = x[:,newaxis]
-    
-    dy = dt/C 
-    Ny = int(round(Ly/dy))
-    y = linspace(0, Ny*dx, Ny+1)
-    dy=y[1]-y[0]
-    yv = y[newaxis,:]
 
-    C1 = (C)**2
-    C2 = (C)**2
-    
-    
+"""
+2D wave equation solved by finite differences::
+  u,xv,yv,t = solver(I,V,f,q,b,Lx,Ly,dt,T,C,q_max,
+                       mode='scalar',bug=None,ue=None):
+Solve the 2D wave equation u_tt + bu_t = (qu_x)x + (qu_y)y + f(x,y,t) 
+on (0,Lx)x(0,Ly) with
+du/dn=0 on the boundary and initial condition du/dt=V(x,y).
+The mesh points are numbered as (0,0), (1,0), (2,0),
+..., (Nx,0), (0,1), (1,1), ..., (Nx, Ny).
+dt is the time step. 
+T is the stop time for the simulation.
+I, V, f are functions: I(x,y), V(x,y), f(x,y,t). V and f
+can be specified as None or 0, resulting in V=0 and f=0.
 
+"""
+def solver(I,V,f,q,b,Lx,Ly,dt,T,C,q_max,mode='scalar',bug=None,ue=None,
+           ani=False):
+
+    
+    
+    Nt = int(round(T/dt))             #Number of timepoints
+    t = linspace(0, Nt*dt, Nt+1)      #t values
+    dt = t[1]-t[0]                    #new dt to fit better  
+    
+    c_max = sqrt(q_max)               #c value for stabilety
+    
+    
+    Nx = int(ceil((Lx*C)/(dt*c_max))) #number of points in x-dir 
+    x = linspace(0,Lx,Nx+1)           #x values  
+    dx=x[1]-x[0]                      #exact dx
+    xv = x[:,newaxis]                 #vectorized axis
+    
+     
+    Ny = int(ceil((Ly*C)/(dt*c_max))) #number of points in y-dir
+    y = linspace(0,Ly,Ny+1)           #y values
+    dy=y[1]-y[0]                      #exact dy
+    yv = y[newaxis,:]                 #vectorized axis
+    
+    #help variables
+    C1 = (dt/dx)**2 
+    C2 = (dt/dy)**2
+    
+    error=0
+    
+    #Define matrixes to store solution
     u = zeros(shape=(Nx+1,Ny+1))
     u_1 = zeros(shape=(Nx+1,Ny+1))
     u_2 = zeros(shape=(Nx+1,Ny+1))
@@ -37,7 +62,7 @@ def solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None,ue=None):
     
     
     
-      
+    #scalar version
     if mode=='scalar':
         
         
@@ -83,6 +108,7 @@ def solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None,ue=None):
                     u[i,j] = bug_4(u_1,x,y,t,f,V,C1,C2,dt,i,j,b,q)
 
         #Neumann condition at the bouandary
+        #Handle edges diffrent in vectorized code.
         u[0,0] = u_1[1,1]
         u[0,Ny] =u_1[1,Ny-1]
         u[Nx,0] = u_1[Nx-1,1]
@@ -106,8 +132,7 @@ def solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None,ue=None):
                 u[0,j],u[Nx,j]=step1_neuman_x(u,x,y,t,f,V,C1,C2,dt,j,Nx+1,b,q)
                 
         
-        print u  
-        print 0
+        
                 
         #fixing u_1 and u_2 for next timestep.
         for i in range(Nx+1):
@@ -115,210 +140,94 @@ def solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None,ue=None):
                 u_2[i,j]=u_1[i,j]
                 u_1[i,j]=u[i,j]
 
+        #time loop for scalar version
+        for n in range(1,Nt):
+        
+            #finding u for t=dt*(n+1) at inner points
+            for i in range(1,Nx):
+                for j in range(1,Ny):
+                    u[i,j] = step(u_1,u_2,x,y,t,f,C1,C2,dt,i,j,n,b,q)
+        
+            #fixing edgy boundary points with Neumann
+            #Handle edges diffrent in vectorized code.
+            u[0,0] = u_1[1,1]
+            u[0,Ny] = u_1[1,Ny-1]
+            u[Nx,0] = u_1[Nx-1,1]
+            u[Nx,Ny] = u_1[Nx-1,Ny-1]
+        
+            #half of boundary with Neumann
+            for i in range(1,Nx):           
+                u[i,0],u[i,Ny]=step_neuman_y(u_1,u_2,x,y,t,f,C1,C2,dt,i,n,Ny+1,b,q)
+            
+            #other half of boundary with Neumann
+            for j in range(1,Ny):           
+                u[0,j],u[Nx,j]=step_neuman_x(u_1,u_2,x,y,t,f,C1,C2,dt,j,n,Nx+1,b,q)
+           
+
+            #fixing u_1 and u_2 for next timestep.
+            for i in range(Nx+1):
+                for j in range(Ny+1):                
+                    u_2[i,j]=u_1[i,j]
+                    u_1[i,j]=u[i,j]
 
 
+
+    #vectorized version
     if mode == 'vector':
+        #animation variable
+        L=[]
+
+
+        #initial condition
         u_1[:,:]=I(xv[:],yv[:])
-        #print u_1
-        error=0
-        u=vec.step1(u,u_1,xv,yv,t,f,V,C1,C2,Nx,Ny,dt,b,q)
+        if ani==True:
+            L.append(u_1)
+
+        #calculating error variable
+        
+        #first step
+        u=vec.step1(u,u_1,xv,yv,t,f,V,C1,C2,Nx,Ny,dt,b,q,x,y)
+        
+        if ani==True:
+            L.append(u)
+            
+        #calculating max norm error at timelevel 1
         if ue!=None:
             err=amax(abs(u-ue(xv[:],yv[:],t[1])))
             if err>error:
                 error =err
         
+        #setting matrixes for next step. 
         u_2[:,:]=u_1[:,:]
         u_1[:,:]=u[:,:]
-        #print u
-        #print 0
+
+        
+            
+            
+
+        #time loop
         for n in range(1,Nt):
-            u=vec.step(u,u_1,u_2,xv,yv,t,f,C1,C2,Nx,Ny,dt,b,q,n)
+            u=vec.step(u,u_1,u_2,xv,yv,t,f,C1,C2,Nx,Ny,dt,b,q,n,x,y)
             u_2[:,:]=u_1[:,:]
             u_1[:,:]=u[:,:]
-            #print u
-            #print t[n]
+            
+            if ani==True:
+                L.append(u)
 
             if ue!=None:
                 err=amax(abs(u-ue(xv[:],yv[:],t[n+1])))
                 if err>error:
                     error =err
-        return u,xv,yv,t,error
 
-
-    for n in range(1,Nt):
         
-        #finding u for t=dt*(n+1) at inner points
-        for i in range(1,Nx):
-            for j in range(1,Ny):
-                u[i,j] = step(u_1,u_2,x,y,t,f,C1,C2,dt,i,j,n,b,q)
-        
-        #fixing edgy boundary points with Neumann
-        u[0,0] = u_1[1,1]
-        u[0,Ny] = u_1[1,Ny-1]
-        u[Nx,0] = u_1[Nx-1,1]
-        u[Nx,Ny] = u_1[Nx-1,Ny-1]
-        
-        #half of boundary with Neumann
-        for i in range(1,Nx):           
-            u[i,0],u[i,Ny]=step_neuman_y(u_1,u_2,x,y,t,f,C1,C2,dt,i,n,Ny+1,b,q)
             
-        #other half of boundary with Neumann
-        for j in range(1,Ny):           
-            u[0,j],u[Nx,j]=step_neuman_x(u_1,u_2,x,y,t,f,C1,C2,dt,j,n,Nx+1,b,q)
-           
+        if ani== True:
+            return L,x,y,t
 
-        #fixing u_1 and u_2 for next timestep.
-        for i in range(Nx+1):
-            for j in range(Ny+1):                
-                u_2[i,j]=u_1[i,j]
-                u_1[i,j]=u[i,j]
-
-
-        #print u
-        #print t[n]
-    return u,xv,yv,t
+        
+        
+    return u,xv,yv,t,error
 
 
 
 
-################################################################
-################################################################
-################################################################
-################################################################
-
-def step1(u_1,x,y,t,f,V,C1,C2,dt,i,j,b,q):
-
-    A = (1+0.5*b*dt)*u_1[i,j] + dt*V(x[i],y[j]) + 0.5*dt**2*f(x[i],y[j],t[0])
-    
-    qx1 = 0.5*(q(x[i],y[j]) + q(x[i+1],y[j]))  
-    qx2 = 0.5*(q(x[i],y[j]) + q(x[i-1],y[j]))
-    
-    B = 0.5*C1*(qx1*(u_1[i+1,j] - u_1[i,j]) - qx2*(u_1[i,j] - u_1[i-1,j])) 
-    
-    qy1 = 0.5*(q(x[i],y[j]) + q(x[i],y[j+1]))  
-    qy2 = 0.5*(q(x[i],y[j]) + q(x[i],y[j-1]))
-    
-    D = 0.5*C2*(qy1*(u_1[i,j+1] - u_1[i,j]) - qy2*(u_1[i,j] - u_1[i,j-1]))
-    
-    return (A + B + D)/( 1 + 0.5*b*dt ) 
-
-def step1_neuman_y(u_1,x,y,t,f,V,C1,C2,dt,i,N,b,q):
-
-    A1 = (1+0.5*b*dt)*u_1[i,0]+dt*V(x[i],y[0])+ 0.5*dt**2*f(x[i],y[0],t[0])
-    A2 = (1+0.5*b*dt)*u_1[i,N-1]+dt*V(x[i],y[N-1])+0.5*dt**2*f(x[i],y[N-1],t[0])
-    
-    q11 = 0.5*(q(x[i],y[0]) + q(x[i+1],y[0]))  
-    q12 = 0.5*(q(x[i],y[0]) + q(x[i-1],y[0]))
-
-    q21 = 0.5*(q(x[i],y[N-1]) + q(x[i+1],y[N-1]))  
-    q22 = 0.5*(q(x[i],y[N-1]) + q(x[i-1],y[N-1]))
-    
-    B1 = 0.5*C1*(q11*(u_1[i+1,0] - u_1[i,0]) - q12*(u_1[i,0] - u_1[i-1,0]))
-    B2 = 0.5*C1*(q21*(u_1[i+1,N-1]-u_1[i,N-1])-q22*(u_1[i,N-1]-u_1[i-1,N-1]))
-
-    D1 = C2*q(x[i],y[0])*(u_1[i,1] - u_1[i,0]) 
-    D2 = C2*q(x[i],y[N-1])*(u_1[i,N-2] - u_1[i,N-1]) 
-
-    return (A1+B1+D1)/( 1 + 0.5*b*dt ), (A2+B2+D2)/( 1 + 0.5*b*dt )
-
-def step1_neuman_x(u_1,x,y,t,f,V,C1,C2,dt,j,N,b,q):
-
-    A1 = (1+0.5*b*dt)*u_1[0,j]+dt*V(x[0],y[j])+0.5*dt**2*f(x[0],y[j],t[0])
-    A2 = (1+0.5*b*dt)*u_1[N-1,j]+dt*V(x[N-1],y[j])+0.5*dt**2*f(x[N-1],y[j],t[0])
-    
-    B1 = C1*q(x[0],y[j])*(u_1[1,j] - u_1[0,j]) 
-    B2 = C1*q(x[N-1],y[j])*(u_1[N-2,j] - u_1[N-1,j]) 
-
-    q11 = 0.5*(q(x[0],y[j]) + q(x[0],y[j+1]))  
-    q12 = 0.5*(q(x[0],y[j]) + q(x[0],y[j-1]))
-
-    q21 = 0.5*(q(x[N-1],y[j]) + q(x[N-1],y[j+1]))  
-    q22 = 0.5*(q(x[N-1],y[j]) + q(x[N-1],y[j-1]))
-
-    D1 = 0.5*C2*(q11*(u_1[0,j+1] - u_1[0,j]) - q12*(u_1[0,j] - u_1[0,j-1]))
-    D2 = 0.5*C2*(q21*(u_1[N-1,j+1] - u_1[N-1,j])-q22*(u_1[N-1,j]-u_1[N-1,j-1]))
-    return (A1+B1+D1)/( 1 + 0.5*b*dt ), (A2+B2+D2)/( 1 + 0.5*b*dt )
-
-def step(u_1,u_2,x,y,t,f,C1,C2,dt,i,j,n,b,q):
-    A = (2+ b*dt)*u_1[i,j] - u_2[i,j] + dt**2*f(x[i],y[j],t[n])
-
-    qx1 = 0.5*(q(x[i],y[j]) + q(x[i+1],y[j]))  
-    qx2 = 0.5*(q(x[i],y[j]) + q(x[i-1],y[j]))
-
-    B = C1*(qx1*(u_1[i+1,j] - u_1[i,j]) - qx2*(u_1[i,j] - u_1[i-1,j]))
-    
-    qy1 = 0.5*(q(x[i],y[j]) + q(x[i],y[j+1]))  
-    qy2 = 0.5*(q(x[i],y[j]) + q(x[i],y[j-1]))
-    
-    D = C2*(qy1*(u_1[i,j+1] - u_1[i,j]) -qy2*(u_1[i,j] - u_1[i,j-1]))
-    
-    return (A + B + D)/(1 + b*dt)
-
-def step_neuman_y(u_1,u_2,x,y,t,f,C1,C2,dt,i,n,N,b,q):
-    A1 = (2 + b*dt)*u_1[i,0] - u_2[i,0] + dt**2*f(x[i],y[0],t[n])
-    A2 = (2 + b*dt)*u_1[i,N-1] - u_2[i,N-1] + dt**2*f(x[i],y[N-1],t[n])
-    
-    q11 = 0.5*(q(x[i],y[0]) + q(x[i+1],y[0]))  
-    q12 = 0.5*(q(x[i],y[0]) + q(x[i-1],y[0]))
-
-    q21 = 0.5*(q(x[i],y[N-1]) + q(x[i+1],y[N-1]))  
-    q22 = 0.5*(q(x[i],y[N-1]) + q(x[i-1],y[N-1]))
-
-    B1 = C1*(q11*(u_1[i+1,0] - u_1[i,0]) - q12*(u_1[i,0] - u_1[i-1,0]))
-    B2  = C1*(q21*(u_1[i+1,N-1]-u_1[i,N-1]) - q22*(u_1[i,N-1]-u_1[i-1,N-1]))
-
-    D1 = 2*C2*q(x[i],y[0])*(u_1[i,1] - u_1[i,0])
-    D2 = 2*C2*q(x[i],y[N-1])*(u_1[i,N-2] - u_1[i,N-1])
-
-    return (A1+B1+D1)/(1 + b*dt), (A2+B2+D2)/(1 + b*dt)
-
-def step_neuman_x(u_1,u_2,x,y,t,f,C1,C2,dt,j,n,N,b,q):
-
-    A1 = (2 + b*dt)*u_1[0,j] - u_2[0,j]+ dt**2*f(x[0],y[j],t[n])
-    A2 = (2 + b*dt)*u_1[N-1,j] - u_2[N-1,j] + dt**2*f(x[N-1],y[j],t[n])
-    
-    B1 = 2*C1*q(x[0],y[j])*(u_1[1,j] - u_1[0,j])
-    B2 = 2*C1*q(x[N-1],y[j])*(u_1[N-2,j] - u_1[N-1,j])
-
-    q11 = 0.5*(q(x[0],y[j]) + q(x[0],y[j+1]))  
-    q12 = 0.5*(q(x[0],y[j]) + q(x[0],y[j-1]))
-
-    q21 = 0.5*(q(x[N-1],y[j]) + q(x[N-1],y[j+1]))  
-    q22 = 0.5*(q(x[N-1],y[j]) + q(x[N-1],y[j-1]))
-
-    D1 = C2*(q11*(u_1[0,j+1]-u_1[0,j])-q12*(u_1[0,j]-u_1[0,j-1]))
-    D2 = C2*(q21*(u_1[N-1,j+1]-u_1[N-1,j])-q22*(u_1[N-1,j]-u_1[N-1,j-1]))
-
-    return (A1+B1+D1)/(1 + b*dt), (A2+B2+D2)/(1 + b*dt)
-
-
-
-
-if __name__ == "__main__":
-    
-    I = lambda x,y: x+y
-    V = lambda x,y: x +0*y
-    f = lambda x,y,t: 0
-    q = lambda x,y: 1
-
-    b=0
-    Lx=1
-    Ly=1
-    dt=0.2
-    T=1
-    C=1
-
-    mode='scalar'
-    u,x,y,t=solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode)
-    print u
-    print 
-
-
-    mode='vector'
-    u,x,y,t,error=solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode)
-    print u
-    print 
-
-"""
-solver(I,V,f,q,b,Lx,Ly,dt,T,C,mode='scalar',bug=None)
-"""
